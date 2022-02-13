567d0ef612ec30a532d168377247719c58b2920b
x0n4d0 <romagnolimauricio@gmail.com>
2022-02-12 23:03:00 -0300

    fix: set new version of apollo module settings

--- school-management/src/app.module.ts
+++ school-management/src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 import { LessonModule } from './lesson/lesson.module';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot({
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
       autoSchemaFile: true,
     }),
     LessonModule,
