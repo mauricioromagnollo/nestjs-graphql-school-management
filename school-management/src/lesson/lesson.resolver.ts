54b68004bb80c18910e53296e95bf52ad866465e
x0n4d0 <romagnolimauricio@gmail.com>
2022-02-13 01:08:35 -0300

    feat: create lessonService and Mutation to create new Lesson

--- school-management/src/lesson/lesson.resolver.ts
+++ school-management/src/lesson/lesson.resolver.ts
@@ -1,9 +1,12 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 
+import { LessonService } from './lesson.service';
 import { LessonType } from './lesson.type';
 
 @Resolver((of) => LessonType)
 export class LessonResolver {
+  constructor(private lessonService: LessonService) {}
+
   @Query((returns) => LessonType)
   lesson() {
     return {
@@ -12,5 +15,14 @@ export class LessonResolver {
       startDate: new Date().toISOString(),
       endDate: new Date().toISOString(),
     };
+  }
+
+  @Mutation((returns) => LessonType)
+  createLesson(
+    @Args('name') name: string,
+    @Args('startDate') startDate: string,
+    @Args('endDate') endDate: string,
+  ) {
+    return this.lessonService.createLesson(name, startDate, endDate);
   }
 }
