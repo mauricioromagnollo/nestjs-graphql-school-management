81fd7a6f4ba78c677f8e476b48df671229c71e30
x0n4d0 <romagnolimauricio@gmail.com>
2022-04-02 01:19:21 -0300

    feat: add lesson type

---
+++ src/lesson/lesson.type.ts
@@ -0,0 +1,16 @@
+import { Field, ObjectType, ID } from '@nestjs/graphql';
+
+@ObjectType('Lesson')
+export class LessonType {
+  @Field((type) => ID)
+  id: string;
+
+  @Field()
+  name: string;
+
+  @Field()
+  startDate: string;
+
+  @Field()
+  endDate: string;
+}
