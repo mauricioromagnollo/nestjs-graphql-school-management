9304fa59643b3a827ab0bd36dbe6565abb396fbc
x0n4d0 <romagnolimauricio@gmail.com>
2022-04-02 10:50:41 -0300

    feat: add Student module

---
+++ src/modules/student/student.module.ts
@@ -0,0 +1,12 @@
+import { Student } from './student.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module } from '@nestjs/common';
+
+import { StudentResolver } from './student.resolver';
+import { StudentService } from './student.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Student])],
+  providers: [StudentResolver, StudentService],
+})
+export class StudentModule {}
---
+++ src/modules/student/student.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+import { Student } from './student.entity';
+import { CreateStudentInput } from './create-student.input';
+
+@Injectable()
+export class StudentService {
+  constructor(
+    @InjectRepository(Student) private studentRepository: Repository<Student>,
+  ) {}
+
+  async getStudent(id: string): Promise<Student> {
+    return this.studentRepository.findOneBy({ id });
+  }
+
+  async getStudents(): Promise<Student[]> {
+    return this.studentRepository.find();
+  }
+
+  async createStudent(
+    createStudentInput: CreateStudentInput,
+  ): Promise<Student> {
+    const { firstName, lastName } = createStudentInput;
+
+    const student = this.studentRepository.create({
+      id: uuid(),
+      firstName,
+      lastName,
+    });
+
+    return this.studentRepository.save(student);
+  }
+}
